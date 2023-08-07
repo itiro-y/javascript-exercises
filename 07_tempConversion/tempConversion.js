7e420dd4cf249c557e2ff6077d534d7918c29b34
Ayrton Itiro Kobo Yoshii <itiro_yoshii@hotmail.com>
2023-08-07 19:40:56 -0300

    Add solutions to ex6-7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(FahrenTemp) {
+  return Math.round((FahrenTemp - 32) * (5/9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(CelsiusTemp) {
+  return Math.round(((CelsiusTemp * 9) / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
